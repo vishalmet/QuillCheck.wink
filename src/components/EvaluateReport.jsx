10cb3af8abfd57ce9fa9a8c30429108c4497eb79
vishalmet <vishalmet003@gmail.com>
2024-09-18 20:09:53 +0530

    evaluate comp

--- src/components/EvaluateReport.jsx
+++ src/components/EvaluateReport.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import Assets from './Assets';
 import Status from './Evaluate/Status';
 import Report from './Evaluate/Report';
+import Info from './Evaluate/Info';
 
 const EvaluateReport = ({ onBackClick }) => {
   return (
     <div
-      className="bg-[#18162099]/60 rounded-[10px] backdrop-filter h-96 backdrop-blur-sm w-[550px] mx-auto jost text-white"
+      className="bg-[#18162099]/60 rounded-[10px] backdrop-filter h-full backdrop-blur-sm w-[550px] mx-auto jost text-white"
       style={{ boxShadow: '4px 4px 12px rgba(0, 0, 0, 0.5)' }}
     >
       <div className="bg-[#181B2E] rounded-t-[10px] p-[15px] px-[20px]">
@@ -16,14 +17,14 @@ const EvaluateReport = ({ onBackClick }) => {
             <p className="text-xl text-center">Higher IMO</p>
             <p className=''>(HIGHER)</p>
           </div>
-          <div className="flex rounded-[20px]">
+          {/* <div className="flex rounded-[20px]">
             <button
               onClick={onBackClick}
               className="bg-[#007AFF] hover:bg-[#007AFF]/70 rounded-[5px] text-white p-2 px-6 text-base border-y border-y-[#86AFFF]"
             >
               Back
             </button>
-          </div>
+          </div> */}
         </div>
         <div className="">
           <p className='text-sm text-white'>0X8B802513D4AA6F349B197A4EA4C26563CD6FD5B2</p>
@@ -32,15 +33,16 @@ const EvaluateReport = ({ onBackClick }) => {
 
       <div className="p-[20px]">
         <div className="flex items-center justify-between h-full">
-          {/* Status Component */}
           <Status />
 
-          {/* Border between Status and Report */}
           <div className="border-l-2 border-white/10 mx-5 self-stretch"></div>
 
-          {/* Report Component */}
           <Report />
         </div>
+
+          <div className="border-b-2 border-white/10 my-5 self-stretch"></div>
+          
+          <Info />
       </div>
     </div>
   );
