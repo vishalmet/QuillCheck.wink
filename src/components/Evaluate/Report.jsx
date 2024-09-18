70d07baa90fd882dcd920d63698b690c1af1cea4
vishalmet <vishalmet003@gmail.com>
2024-09-18 11:37:23 +0530

    component ui

--- src/components/Evaluate/Report.jsx
+++ src/components/Evaluate/Report.jsx
@@ -1,9 +1,41 @@
 import React from 'react'
+import Assets from '../Assets'
 
 const Report = () => {
-  return (
-    <div>Report</div>
-  )
+    return (
+        <div>
+            <div className=" space-y-2">
+                <div className="flex justify-between space-x-9 items-center">
+                    <div className=" flex items-center gap-2">
+                        <img className=' h-4' src={Assets.XVoilet} alt="" />
+                        <p className=' '>Critical:</p>
+                    </div>
+                    <p>1</p>
+                </div>
+                <div className="flex justify-between space-x-9 items-center">
+                    <div className=" flex items-center gap-2">
+                        <img className=' h-4' src={Assets.Caution} alt="" />
+                        <p className=' '>Risky:</p>
+                    </div>
+                    <p>12</p>
+                </div>
+                <div className="flex justify-between space-x-9 items-center">
+                    <div className=" flex items-center gap-2">
+                        <img className=' h-4' src={Assets.Exclamatory} alt="" />
+                        <p className=' '>Exclamatory:</p>
+                    </div>
+                    <p>0</p>
+                </div>
+                <div className="flex justify-between space-x-9 items-center">
+                    <div className=" flex items-center gap-2">
+                        <img className=' h-4' src={Assets.Dot} alt="" />
+                        <p className=' '>Neutral:</p>
+                    </div>
+                    <p>7</p>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Report
