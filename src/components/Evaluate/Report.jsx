224525025c76525035da7dfb2a7891ef80916adb
vishalmet <vishalmet003@gmail.com>
2024-09-27 15:02:32 +0530

    data display

--- src/components/Evaluate/Report.jsx
+++ src/components/Evaluate/Report.jsx
@@ -1,41 +1,41 @@
-import React from 'react'
-import Assets from '../Assets'
+import React from 'react';
+import Assets from '../Assets';
 
-const Report = () => {
+const Report = ({ critical, risky, medium, neutral }) => {
     return (
         <div>
-            <div className=" space-y-2 text-sm">
+            <div className="space-y-2 text-sm">
                 <div className="flex justify-between space-x-9 items-center">
-                    <div className=" flex items-center gap-2">
-                        <img className=' h-4' src={Assets.XVoilet} alt="" />
-                        <p className=' '>Critical:</p>
+                    <div className="flex items-center gap-2">
+                        <img className='h-4' src={Assets.XVoilet} alt="" />
+                        <p>Critical:</p>
                     </div>
-                    <p>1</p>
+                    <p>{critical}</p>
                 </div>
                 <div className="flex justify-between space-x-9 items-center">
-                    <div className=" flex items-center gap-2">
-                        <img className=' h-4' src={Assets.Caution} alt="" />
-                        <p className=' '>Risky:</p>
+                    <div className="flex items-center gap-2">
+                        <img className='h-4' src={Assets.Caution} alt="" />
+                        <p>Risky:</p>
                     </div>
-                    <p>12</p>
+                    <p>{risky}</p>
                 </div>
                 <div className="flex justify-between space-x-9 items-center">
-                    <div className=" flex items-center gap-2">
-                        <img className=' h-4' src={Assets.Exclamatory} alt="" />
-                        <p className=' '>Medium:</p>
+                    <div className="flex items-center gap-2">
+                        <img className='h-4' src={Assets.Exclamatory} alt="" />
+                        <p>Medium:</p>
                     </div>
-                    <p>0</p>
+                    <p>{medium}</p>
                 </div>
                 <div className="flex justify-between space-x-9 items-center">
-                    <div className=" flex items-center gap-2">
-                        <img className=' h-4' src={Assets.Dot} alt="" />
-                        <p className=' '>Neutral:</p>
+                    <div className="flex items-center gap-2">
+                        <img className='h-4' src={Assets.Dot} alt="" />
+                        <p>Neutral:</p>
                     </div>
-                    <p>7</p>
+                    <p>{neutral}</p>
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Report
+export default Report;
